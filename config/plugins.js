02cfef0ff11fe64a9748324a00eabfd79de7722d
torrac12 <aiyuai1987@163.com>
2023-03-25 18:55:14 +0800

    temp

---
+++ config/plugins.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "measurement-protocol": {
+    config: {
+      apiSecret: '2R325C6NTDe8dMJnJmNrpg',
+      measurementId: 'G-G5F9JM5HZ9',
+      useValidationServer: false,
+    }
+  },
+}
