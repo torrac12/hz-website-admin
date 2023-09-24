53ddc5fbcf6246df46f4c7e5047df15820d4b725
torrac12 <aiyuai1987@163.com>
2023-09-24 14:19:09 +0800

    update comment

--- src/api/comment/content-types/comment/lifecycles.js
+++ src/api/comment/content-types/comment/lifecycles.js
@@ -1,20 +1,20 @@
 module.exports = {
-  async beforeUpdate(event) {
-    const { params } = event;
-    const { where, data } = params;
-    const currentData = await strapi.entityService.findOne("api::comment.comment", where.id);
+  // async beforeUpdate(event) {
+  //   const { params } = event;
+  //   const { where, data } = params;
+  //   const currentData = await strapi.entityService.findOne("api::comment.comment", where.id);
 
-    if (data.addToPost === true && currentData.addToPost === false) {
-      const { content, title } = data;
-      const a = await strapi.entityService.create("api::post.post", {
-        data: {
-          content,
-          title,
-          chinese: true
-        }
-      })
+  //   if (data.addToPost === true && currentData.addToPost === false) {
+  //     const { content, title } = data;
+  //     const a = await strapi.entityService.create("api::post.post", {
+  //       data: {
+  //         content,
+  //         title,
+  //         chinese: true
+  //       }
+  //     })
 
-      console.log(a)
-    }
-  },
+  //     console.log(a)
+  //   }
+  // },
 };
